ce6416f5d8e239b5cc789fbdf7cb8250e1b778c6
Resume Website <jdcollins242@gmail.com>
2021-08-19 00:01:59 -0400

    Created footer. Added colors to style guide

--- js/navbar.js
+++ js/navbar.js
@@ -1,19 +1,29 @@
-Vue.component("navbar", {
-    template: `<nav>
-                    <img src="../assets/img/home_page/header/logo.png" width="200">
-                    <div class="nav-links">
-                        <li><a href="#" class="nav-link">Work</a></li>
-                        <li><a href="#" class="nav-link">Services</a></li>
-                        <li><a href="#" class="nav-link">About</a></li>
-                        <li><a href="#" class="nav-link">Clients</a></li>
-                        <button class="btn btn-call-sm">Book A Call</button>
-                    </div>
-            </nav> `,
+Vue.component("navbar-component", {
+    template: `
+    <nav>
+        <a href="../index.html">
+            <img id="logo" src="../assets/img/home_page/header/logo.png" alt="Plato Alpha Logo" width="200">
+        </a>
+        <div class="nav-links">
+            <li>
+                <a href="#" class="nav-link">Work</a>
+            </li>
+            <li>
+                <a href="#" class="nav-link">Services</a>
+            </li>
+            <li>
+                <a href="#" class="nav-link">About</a>
+            </li>
+            <li>
+                <a href="#" class="nav-link">Clients</a>
+            </li>
+            <button class="btn btn-call-sm">Book A Call</button>
+        </div>
+    </nav> `,
 });
 
 var app = new Vue({
-    
-    el: "#app",
+    el: "#navbar-component",
     data: {
         message: "Hello Vue!",
     },
--- style-guide.md
+++ style-guide.md
@@ -1,15 +1,24 @@
-## Layout
+### Layout ###
 
 
-## Colors
+### Colors ###
 Dark Blue: rgb(0, 31, 99)
 Light Blue: #267CF1
 Light Gray: #8c8c8c
 
-## Fonts
+# Footer Background Colors
+Footer 1: #21212B
+Footer 2: #1D1D25
+Footer 3: #15151F
+
+### Fonts ###
 
 
-## Font Sizes
+# Font Sizes
+
+### Font Awesome CDN ###
+<script src="https://kit.fontawesome.com/c34c8ba824.js" crossorigin="anonymous"></script>
 
 
-## Media Query Breakpoints
+
+### Media Query Breakpoints ###
