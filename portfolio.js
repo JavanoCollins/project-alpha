841f31b8a12d4b4c3e105ffa0265a0ffb1da7193
DarringtonRicardo <darrin.kemp@gmail.com>
2021-08-21 19:47:38 -0400

    update

---
+++ portfolio.js
@@ -0,0 +1,51 @@
+const hoverMe = document.querySelector(".parent-hover")
+const card = document.querySelector(".card");
+const cardContainer = document.querySelector(".card-container");
+
+
+cardContainer.addEventListener('mouseover', (e) => {
+  hoverMe.classList.toggle("on-hover")
+  console.alert(hoverMe);
+});
+card.addEventListener('click', () => {
+    search.classList.toggle('active')
+})
+
+
+// //Movement Animation to happen
+// const card = document.querySelector(".card");
+// const container = document.querySelector(".container");
+// //Items
+// const title = document.querySelector(".title");
+// const sneaker = document.querySelector(".sneaker img");
+// const purchase = document.querySelector(".purchase");
+// const description = document.querySelector(".info h3");
+// const sizes = document.querySelector(".sizes");
+
+// //Moving Animation Event
+// container.addEventListener("mousemove", (e) => {
+//   let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+//   let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+//   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+// });
+// //Animate In
+// container.addEventListener("mouseenter", (e) => {
+//   card.style.transition = "none";
+//   //Popout
+//   title.style.transform = "translateZ(50px)";
+//   sneaker.style.transform = "translateZ(20px) rotateZ(-45deg)";
+//   description.style.transform = "translateZ(25px)";
+//   sizes.style.transform = "translateZ(10px)";
+//   purchase.style.transform = "translateZ(15px)";
+// });
+// //Animate Out
+// container.addEventListener("mouseleave", (e) => {
+//   card.style.transition = "all 0.5s ease";
+//   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+//   //Popback
+//   title.style.transform = "translateZ(0px)";
+//   sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+//   description.style.transform = "translateZ(0px)";
+//   sizes.style.transform = "translateZ(0px)";
+//   purchase.style.transform = "translateZ(0px)";
+// });
