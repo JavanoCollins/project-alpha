963bfda6902b859de8e05ec7b77586e03a3c2fb1
Resume Website <jdcollins242@gmail.com>
2021-08-22 18:51:09 -0400

    Commit changes

--- js/homepage/header.js
+++ js/homepage/header.js
@@ -1,4 +1,12 @@
+// --- DOM Selectors ---
 const clientsImgContainer = document.querySelector(".clients-img-container");
+const dropdownMenu = document.querySelector(".dropdown-content");
+const serviceArrow = document.querySelector(".fa-chevron-down");
+
+// Nav Links
+const serviceLink = document.querySelector("#services");
+
+
 
 const clientImages = [
     "sixnfour",
@@ -10,7 +18,54 @@ const clientImages = [
     "peepx",
 ];
 
+const showHideServiceArrow = () => {
+    serviceArrow.classList.toggle("rotateArrow")
+}
+
+serviceLink.addEventListener("click", () => {
+    dropdownMenu.classList.toggle("show-element");
+    showHideServiceArrow();
+    dropdownMenu.innerHTML = `
+                        <div class="service">
+                            <i class="fas fa-laptop-code"></i>
+                            <h3>Software Development</h3>
+                            <p>
+                                Lorem ipsum, dolor sit amet
+                                consectetur adipisicing.
+                            </p>
+                        </div>
+                        <div class="service">
+                            <h3>System Integration</h3>
+                            <p>
+                                Lorem ipsum, dolor sit amet
+                                consectetur adipisicing.
+                            </p>
+                        </div>
+                        <div class="service">
+                            <h3>Ecommerce</h3>
+                            <p>
+                                Lorem ipsum, dolor sit amet
+                                consectetur adipisicing.
+                            </p>
+                        </div>
+                        <div class="service">
+                            <h3>Maintenance</h3>
+                            <p>
+                                Lorem ipsum, dolor sit amet
+                                consectetur adipisicing.
+                            </p>
+                        </div>
+                        <div class="service">
+                            <h3>Website Design</h3>
+                            <p>
+                                Lorem ipsum, dolor sit amet
+                                consectetur adipisicing.
+                            </p>
+                        </div>
+    `;
+});
+
 // Render client logo images to the client section
 clientImages.map((img) => {
-    return clientsImgContainer.innerHTML += `<img class="client-img ${img}" src="../../assets/img/home_page/clients/${img}.png">`
-})
+    return (clientsImgContainer.innerHTML += `<img class="client-img ${img}" src="../../assets/img/home_page/clients/${img}.png">`);
+});
--- js/navbar.js
+++ js/navbar.js
@@ -1,56 +1,16 @@
 Vue.component("nav-link", {
-    props: ["link", "dropdown"],
+    props: ["title", "link", "dropdown"],
     template: `<li>
-                <a v-if="!dropdown" href="#" class="nav-link">
-                    {{link}}
+                <a v-if="!dropdown" href="#" class="nav-link" :id="link">
+                    {{title}}
                 </a>
                 <div v-if="dropdown" class="dropdown">
                     <div class="service-arrow">
-                        <a href="#" class="dropbtn nav-link"
-                            >{{link}}</a
+                        <a href="#" class="dropbtn nav-link" :id="link"
+                            >{{title}}</a
                         >
                         <i class="fas fa-chevron-down"></i>
                     </div>
-
-                    <div class="dropdown-content">
-                        <div class="service">
-                            <i class="fas fa-laptop-code"></i>
-                            <h3>Software Development</h3>
-                            <p>
-                                Lorem ipsum, dolor sit amet
-                                consectetur adipisicing.
-                            </p>
-                        </div>
-                        <div class="service">
-                            <h3>System Integration</h3>
-                            <p>
-                                Lorem ipsum, dolor sit amet
-                                consectetur adipisicing.
-                            </p>
-                        </div>
-                        <div class="service">
-                            <h3>Ecommerce</h3>
-                            <p>
-                                Lorem ipsum, dolor sit amet
-                                consectetur adipisicing.
-                            </p>
-                        </div>
-                        <div class="service">
-                            <h3>Maintenance</h3>
-                            <p>
-                                Lorem ipsum, dolor sit amet
-                                consectetur adipisicing.
-                            </p>
-                        </div>
-                        <div class="service">
-                            <h3>Website Design</h3>
-                            <p>
-                                Lorem ipsum, dolor sit amet
-                                consectetur adipisicing.
-                            </p>
-                        </div>
-                    </div>
-                    
                 </div>
             </li>`,
 });
@@ -65,48 +25,3 @@ var app = new Vue({
         console.log(this.services);
     },
 });
-
-{
-    /* <nav>
-        <a href="../index.html">
-            <img id="logo" src="../assets/img/home_page/header/logo.png" alt="Plato Alpha Logo" width="200">
-        </a>
-        <div class="nav-links">
-            <li>
-                <a href="#" class="nav-link">Work</a>
-            </li>
-            <li>
-                <div class="dropdown">
-                    <a href="#" class="dropbtn nav-link">Services</a>
-                    <div class="dropdown-content">
-                        <div class="service">
-                            <p>Software Development</p>
-                        </div>
-                        <div class="service">
-                            <p>Software Development</p>
-                        </div>
-                        <div class="service">
-                            <p>Software Development</p>
-                        </div>
-                        <div class="service">
-                            <p>Software Development</p>
-                        </div>
-                        <div class="service">
-                            <p>Software Development</p>
-                        </div>
-                        <div class="service">
-                            <p>Software Development</p>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <li>
-                <a href="#" class="nav-link">About</a>
-            </li>
-            <li>
-                <a href="#" class="nav-link">Clients</a>
-            </li>
-            <button class="btn btn-call-sm">Book A Call</button>
-        </div>
-    </nav> */
-}
